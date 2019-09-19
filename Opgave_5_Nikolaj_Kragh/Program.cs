84cbc83251eca381a0f2444107151b7e3cddd389
NKragh <39466838+NKragh@users.noreply.github.com>
2019-09-19 11:46:50 +0200

    Add project files.

---
+++ Opgave_5_Nikolaj_Kragh/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Opgave_5_Nikolaj_Kragh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
