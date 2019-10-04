163cc169d73a2c7272c74e9e24f1af88e13ffb70
NKragh <39466838+NKragh@users.noreply.github.com>
2019-10-04 11:58:37 +0200

    Rewritten
    To two line input

--- Opgave_5_Nikolaj_Kragh/ServerWorker.cs
+++ Opgave_5_Nikolaj_Kragh/ServerWorker.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 using Opgave_1_Nikolaj_Kragh;
 
 namespace Opgave_5_Nikolaj_Kragh
@@ -40,34 +41,39 @@ namespace Opgave_5_Nikolaj_Kragh
             using (StreamReader sr = new StreamReader(socket.GetStream()))
             using (StreamWriter sw = new StreamWriter(socket.GetStream()))
             {
+
+
                 string str = sr.ReadLine();
+                string str2 = sr.ReadLine();
 
-                string[] resarr = str.Split('/');
-
-                switch (resarr.Length)
+                //string[] resarr = str.Split('/');
+                if (String.IsNullOrEmpty(str))
                 {
-                    case 1:
-                        BookList.ForEach(sw.WriteLine);
-                        break;
-                    default:
-                        //sw.WriteLine(resarr[0]);
-                        switch (resarr[0].ToLower())
-                        {
-                            case "hent":
-                                sw.WriteLine(BookList.Find(x => x.Isbn13 == resarr[1]));
-                                break;
-                            case "gem":
-                                // Gem/Titel/Forfatter/Sidetal/ISBN13
-                                BookList.Add(new Book(resarr[1], resarr[2], Int32.Parse(resarr[3]), resarr[4]));
-                                sw.WriteLine("This far");
-                                break;
-                            default:
-                                sw.WriteLine("Fejlagtigt input.");
-                                break;
-                        }
-                        break;
+                    sw.WriteLine("Input er tomt. Prøv igen.");
                 }
+                else
+                {
 
+                    switch (str.ToLower())
+                    {
+                        case "hentalle":
+                            BookList.ForEach(sw.WriteLine);
+                            break;
+                        case "hent":
+                            sw.WriteLine(BookList.Find(x => x.Isbn13 == str2));
+                            break;
+                        case "gem":
+                            // Gem/Titel/Forfatter/Sidetal/ISBN13
+                            JsonConvert.DeserializeObject(str2);
+                            BookList.Add(JsonConvert.DeserializeObject<Book>(str2));
+                            //BookList.Add(new Book(resarr[1], resarr[2], Int32.Parse(resarr[3]), resarr[4]));
+                            sw.WriteLine("Book saved!");
+                            break;
+                        default:
+                            sw.WriteLine("Fejlagtigt input.");
+                            break;
+                    }
+                }
                 sw.Flush();
             }
         }
